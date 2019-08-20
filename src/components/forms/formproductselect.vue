797f7345f8e3301f42565795c21b11bc42f0f0b7
andrejsarunov <dev@comformo.com>
2019-08-20 13:28:07 +0300

    upload cabinet

--- src/components/forms/formproductselect.vue
+++ src/components/forms/formproductselect.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="input_group_select">
-        <Multiselect v-model="value" :options="options" :placeholder="placeholder" :label="selectLabelName" :track-by="selectLabelIndex" :searchable="searchable" @input="onSelect" :class="[value!='' ? 'ok' : 'err']" :multiple="multiple" :close-on-select="closeOnSelect" :hideSelected="hideSelected"></Multiselect>
+        <Multiselect v-model="value" :options="valueOption" :placeholder="placeholder" label="label" track-by="index" :searchable="true" @input="onSelect" :class="[value!='' ? 'ok' : 'err']" :multiple="multiple" :close-on-select="closeOnSelect" :hideSelected="hideSelected"></Multiselect>
         <label class="label-up" style="position: absolute;top: -6px;left: 6px;font-family: 'Brandon Grotesque';font-weight: 500;font-size: .8em;letter-spacing: .05em;line-height: .8em;text-align: left;z-index: 1;color: #2f2f2f;background-color: #fff;">{{labelName}}</label>
     </div>
 </template>
@@ -11,10 +11,10 @@
     export default {
         name: "formproductselect",
         components:{Multiselect,},
-        date(){
+        data(){
             return{
                 value: null,
-                options: [
+                valueOption: [
                     {label: 'Company', index: 1},
                     {label: 'Bank Account', index: 2},
                     {label: 'Structure',index: 3},
@@ -31,18 +31,6 @@
             placeholder: {
                 type: String,
                 default: 'Select one',
-            },
-            selectLabelName: {
-                type: String,
-                default: 'label',
-            },
-            selectLabelIndex: {
-                type: String,
-                default: 'index',
-            },
-            searchable: {
-                type: Boolean,
-                default: true
             },
             multiple: {
                 type: Boolean,
