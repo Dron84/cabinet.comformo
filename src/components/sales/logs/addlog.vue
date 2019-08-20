8828a68bb987bed0f221582507d6e59b06dadff3
andrejsarunov <dev@comformo.com>
2019-08-20 15:59:47 +0300

    add log component complete

--- src/components/sales/logs/addlog.vue
+++ src/components/sales/logs/addlog.vue
@@ -15,16 +15,19 @@
                     <div class="modal-container">
                         <button type="button" class="close" @click=" modal = false">&times;</button>
                             <div class="modal-body">
-                                <formselect labelName="User" :options="users" @selected="selectUser($event)" :class="[selectedUser!='' ? 'ok': 'err']"></formselect>
-                                <forminput labelName="Description" @inputModel="descriptionInput($event)" :class="[description!='' ? 'ok': 'err']"></forminput>
+                                <formselect labelName="User" :options="users" @selected="selectUser($event)"></formselect>
+                                <forminput labelName="Description" @inputModel="descriptionInput($event)"></forminput>
                                 <div class="input-wrapper">
-                                    <date class="form-control" id="dateTo" v-model="dateTo" :class="[dateTo!='' ? 'ok': 'err']"></date>
+                                    <date class="form-control valid" id="dateTo" v-model="dateTo"></date>
                                     <label for="dateTo" class="label-up">Date to Contact</label>
                                 </div>
                                 <formproductselect :hideSelected="true" @selectedProduct="selectProduct($event)" labelName="Product" :placeholder="''" :multiple="true" :closeOnSelect="false"></formproductselect>
                             </div>
                         <div class="centrize" style="width: 200px;">
-                            <button type="button" class="submit"  style="background-image: -webkit-linear-gradient( 128deg, rgb(255,136,0) 0%, rgb(251,159,53) 0%, rgb(255,136,0) 97%);width: 100%;" @click="addlog()">Add Log</button>
+                            <button v-if="addStatus==''" type="button" class="submit"  style="background-image: -webkit-linear-gradient( 128deg, rgb(255,136,0) 0%, rgb(251,159,53) 0%, rgb(255,136,0) 97%);width: 100%;" @click="addlog()">Add Log</button>
+                            <button v-else-if="addStatus=='wait'" type="button" class="submit"  style="background-image: -webkit-linear-gradient( 128deg, rgb(255,136,0) 0%, rgb(251,159,53) 0%, rgb(255,136,0) 97%);width: 100%;">Wait
+                                <img src="/static/img/loading.gif" alt="" height="20"></button>
+                            <button v-else-if="addStatus=='added'" type="button" class="submit"  style="background-image: -webkit-linear-gradient( 128deg, rgb(255,136,0) 0%, rgb(251,159,53) 0%, rgb(255,136,0) 97%);width: 100%;">Log is Added</button>
                         </div>
                     </div>
                 </div>
@@ -54,6 +57,7 @@
                 description: '',
                 dateTo: '',
                 selectedProduct:{},
+                addStatus: '',
             }
         },
         methods: {
@@ -85,6 +89,7 @@
             },
             addlog(){
                 console.log(this.selectedUser,this.selectedProduct,this.description, this.customFormatter(this.dateTo))
+                this.addStatus = 'wait'
                 let fd = new FormData
                 fd.append('userId', this.selectedUser.index)
                 fd.append('productlist',JSON.stringify(this.selectedProduct))
@@ -92,9 +97,17 @@
                 fd.append('dateToContact',this.customFormatter(this.dateTo))
                 fd.append('log','add')
                 axios.post(this.$store.getters.getPostUrl, fd).then(res=>{
-                    console.log(res.data)
+                    // console.log(res.data)
+                    if(res.data.mess == 'Sql INSERT is ok'){
+                        this.addStatus = 'added'
+                        setTimeout(()=>{
+                            this.addStatus = ''
+                            this.modal = false
+                            this.dateTo=''
+                        },5000)
+                    }
                 })
-            }
+            },
         },
         created(){
             this.createUsersList()
